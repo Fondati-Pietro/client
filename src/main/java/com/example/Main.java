fe4b0f4ed12e4c3e7e06a988f6b3a2198bff2f8c
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:32:02 +0200

    secondo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownError, UnknownHostException, IOException{
+        System.out.println("Client partito");
+
+        Socket server = new Socket("localhost", 3000);
+
+        System.out.println("Client collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        DataOutputStream out = new DataOutputStream(server.getOutputStream());
+
+        out.writeBytes("ciao" + '\n');
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("Stringa ricevuta: " + stringaRicevuta);
+
+        server.close();
     }
 }
